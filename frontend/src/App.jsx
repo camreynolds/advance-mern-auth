60d5c414d3dea31c5470a8f8c19aa3112502038d
carlos <camr.personal@gmail.com>
2025-01-15 17:41:30 -0500

    issue #11

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import {Routes,Route} from "react-router-dom"
 import FloatingShape from "./components/FloatingShape"
 import SignupPage from "./pages/SignupPage"
 import LoginPage from "./pages/LoginPage"
+import EmailVerificationPage from "./pages/EmailVerificationPage"
 
 function App() {
   
@@ -48,6 +49,7 @@ function App() {
             <Route path="/" element={"Home"}/>
             <Route path="/signup" element={<SignupPage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/verify-email" element={<EmailVerificationPage/>}/>
           </Routes>
         </div>
     </>
